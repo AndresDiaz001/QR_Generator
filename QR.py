6339c97d928d8a5bdaee064b3eac0100b6fa1619
Andes Diaz <53575123+AndresDiaz001@users.noreply.github.com>
2023-09-18 17:40:50 -0500

    update visual form

--- QR.py
+++ QR.py
@@ -2,17 +2,47 @@
 # mediante --> pip install qrcode en consola
 
 # importamos la libreria
+import tkinter as tk
 import qrcode
 
-# se le pide al usuario su nombre
-nombre = input("Digita tu nombre: ")
-# se realiza un saludo
-print(f"Hola {nombre}")
-# se le pide la cadena que desea usar en la convercion
-data = input("Digita la cadena para convertir a QR: ")
-# con la funcion make(crear) se realiza la conversion de cadena a qr
-img = qrcode.make(data)
-# por ultimo se guarda la imagen con su respectiva extencion
-img.save('qr_image.png')
-# se entrega mensaje de confirmacion
-print("Revisa la carpeta del proyecto que ya se genero el QR")
+def generar_codigo():
+    # Obtiene el texto del cuadro de entrada
+    data = texto.get()
+
+    # Genera el código QR
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=25,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    # Crea una imagen PIL desde el código QR
+    imagen = qr.make_image(fill_color="black", back_color="white")
+
+    # Muestra la imagen en la ventana
+    imagen.show()
+
+    imagen.save('qr_image.png')
+
+# Crea una ventana
+ventana = tk.Tk()
+ventana.title("Generador de Códigos QR")
+ventana.geometry("200x150")
+
+# Etiqueta
+etiqueta = tk.Label(ventana, text="Ingrese el Texto para el código QR:")
+etiqueta.pack()
+
+# Cuadro de entrada
+texto = tk.Entry(ventana)
+texto.pack()
+
+# Botón para generar el código QR
+boton_generar = tk.Button(ventana, text="Generar", command=generar_codigo)
+boton_generar.pack()
+
+# Ejecuta la aplicación
+ventana.mainloop()
